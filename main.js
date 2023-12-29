16173a9c1ed41cf55da0347a96594a3de6ff7dd4
Omkeshwar Gupta <om999g@gmail.com>
2023-12-29 10:16:45 +0530

    done

---
+++ main.js
@@ -0,0 +1,10 @@
+
+AOS.init();
+var btn = document.getElementById("btn");
+var nav = document.getElementById("menu");
+if (btn && nav) {
+    btn.addEventListener("click", function () {
+        nav.classList.toggle("flex");
+        nav.classList.toggle("hidden");
+    });
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+      current: 'currentColor',
+        primary: "#ff9142",
+      },
+    },
+    fontFamily: {
+      rubik: ["Rubik", "sans-serif"],
+      syne: ["Syne", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
+
